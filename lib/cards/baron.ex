881deb35e420f4a873a8eb26bcf68d5c0ccb07e7
Scott Riley <scott@imajica.com>
2017-01-02 12:01:02 +0000

    Adds baron card with tests

---
+++ lib/cards/baron.ex
@@ -0,0 +1,13 @@
+defmodule LoveLetter.Card.Baron do
+
+  def run(scores, {current, target}) do
+    current_value = Map.get(scores, current)
+    target_value = Map.get(scores, target)
+    cond do
+      current_value > target_value -> Map.delete(scores, target)
+      current_value < target_value -> Map.delete(scores, current)
+      :else -> scores
+    end
+  end
+
+end
---
+++ test/cards/baron_test.exs
@@ -0,0 +1,24 @@
+defmodule LoveLetterCardBaronTest do
+  use ExUnit.Case
+
+  test "current player has higher value" do
+    scores = %{1 => 2, 2 => 5, 3 => 7}
+    choice = {1, 2}
+    result = LoveLetter.Card.Baron.run(scores, choice)
+    assert result == %{2 => 5, 3 => 7}
+  end
+
+  test "target player has higher value" do
+    scores = %{1 => 2, 2 => 5, 3 => 7}
+    choice = {3, 2}
+    result = LoveLetter.Card.Baron.run(scores, choice)
+    assert result == %{1 => 2, 3 => 7}
+  end
+
+  test "neither player has higher value" do
+    scores = %{1 => 2, 2 => 2, 3 => 7}
+    choice = {1, 2}
+    result = LoveLetter.Card.Baron.run(scores, choice)
+    assert result == %{1 => 2, 2 => 2, 3 => 7}
+  end
+end
