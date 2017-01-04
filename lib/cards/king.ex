0d1c0908ff9b615b3623fb0c880e3a6012c057bc
Scott Riley <scott.riley@cultivatehq.com>
2017-01-04 15:43:43 +0000

    Adds king card with tests

---
+++ lib/cards/king.ex
@@ -0,0 +1,9 @@
+defmodule LoveLetter.Card.King do
+
+  def run(scores, {current, target}) do
+    current_card = scores[current]
+    target_card = scores[target]
+    %{ scores | current => target_card, target => current_card }
+  end
+
+end
---
+++ test/cards/king_test.exs
@@ -0,0 +1,10 @@
+defmodule LoveLetterCardKingTest do
+  use ExUnit.Case
+
+  test "causes two players to swap hands" do
+    scores = %{1 => 2, 2 => 5, 3 => 7}
+    choice = {1, 3}
+    result = LoveLetter.Card.King.run(scores, choice)
+    assert result == %{1 => 7, 2 => 5, 3 => 2}
+  end
+end
